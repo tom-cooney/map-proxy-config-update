47bbd946b5e4ffbad960d7d96aef2d7574516fc1
tom-cooney <tom.cooney@mail.utoronto.ca>
2020-11-16 08:41:41 -0500

    firsat commit

---
+++ update-mapproxy-layer-dimensions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+from owslib.wms import WebMapService
+import yaml
+
+layer_name = sys.argv[1]
+mapproxy_config = sys.argv[2]
+
+url = 'https://geo.weather.gc.ca/geomet?layer={}'.format(layer_name)
+
+wms = WebMapService(url, version='1.3.0')
+
+dimensions = {}
+
+for key, value in wms[layer_name].dimensions.items():
+    if key == 'time':
+        key2 = key
+    else:
+        key2 = 'dim_{}'.format(key)
+
+    dimensions[key2] = {
+        'default': value['default'],
+        'values': value['values']
+    }
+
+if not dimensions:
+    print('No dimensions found')
+    sys.exit(1)
+
+with open(mapproxy_config) as fh:
+    dict_ = yaml.load(fh, Loader=yaml.FullLoader)
+
+    for layer in dict_['layers']:
+        if layer['name'] == layer_name:
+            for key, value in dimensions.items():
+                layer['dimensions'][key] = value
+
+    print(yaml.dump(dict_))
+
