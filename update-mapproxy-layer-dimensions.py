d726c76e2cba039e8588c1e4321ecedd9bcb90e1
tom-cooney <tom.cooney@mail.utoronto.ca>
2020-12-23 13:58:00 +0000

    changing

--- update-mapproxy-layer-dimensions.py
+++ update-mapproxy-layer-dimensions.py
@@ -1,3 +1,32 @@
+# =================================================================
+#
+# Author: Tom Cooney <tom.cooney@canada.ca>
+#
+# Copyright (c) 2020 Tom Cooney
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
 import click
 import os
 
@@ -19,7 +48,7 @@ def update_config(mapproxy_config, layers_to_update):
     return dict_
 
 
-def wms_update_layer_dimensions(input_layers, mapproxy_config):
+def update_from_wms(input_layers, mapproxy_config):
     layer_list = input_layers.split(",")
     layers_to_update = {}
     for layer_name in layer_list:
@@ -40,7 +69,7 @@ def wms_update_layer_dimensions(input_layers, mapproxy_config):
 
     return dict_
 
-def xml__update_layer_dimensions(input_layers, mapproxy_config):
+def update_from_xml(input_layers, mapproxy_config):
     layer_list = input_layers.split(",")
     layers_to_update = {}
     with open('/data/web/geomet2-nightly/latest/build/conf/geomet-wms-1.3.0-capabilities-en.xml', 'rb') as fh:
@@ -64,7 +93,7 @@ def xml__update_layer_dimensions(input_layers, mapproxy_config):
     return dict_
 
 
-def mapfile_update_layer_dimensions(input_layers, mapproxy_config, MAPPROXY_MAPFILE_DIR):
+def update_from_mapfile(input_layers, mapproxy_config, MAPPROXY_MAPFILE_DIR):
     layer_list = input_layers.split(",")
     layers_to_update = {}
     for layer_name in layer_list:
@@ -93,22 +122,20 @@ def mapfile_update_layer_dimensions(input_layers, mapproxy_config, MAPPROXY_MAPF
 #@click.pass_context
 @click.option('--input_layers', 'input_layers', help='layers to update dimensions of in mapproxy config')
 @click.option('--mapproxy_config', 'mapproxy_config', help='mapproxy config to update layers and dimensions')
-@click.option('--mode', 'mode', help='select either wms or mapfile')
+@click.option('--mode', 'mode', type=click.Choice(['wms', 'xml', 'mapfile']), default='wms', help='select either wms or mapfile')
 def cli(input_layers, mapproxy_config, mode):
     if 'wms' in mode:
-        output_dict = wms_update_layer_dimensions(input_layers, mapproxy_config)
+        output_dict = update_from_wms(input_layers, mapproxy_config)
     elif 'xml' in mode:
-        output_dict = xml__update_layer_dimensions(input_layers, mapproxy_config)
+        output_dict = update_from_xml(input_layers, mapproxy_config)
     elif 'mapfile' in mode:
         # if the MAPPROXY_MAPFILE_DIR environment variable doesn't exist it will set it to None by default
+        # in bash `export MAPPROXY_MAPFILE_DIR=/data/web/geomet2-nightly/latest/build/conf/`
         MAPPROXY_MAPFILE_DIR = os.getenv('MAPPROXY_MAPFILE_DIR', None)
-        output_dict = mapfile_update_layer_dimensions(input_layers, mapproxy_config, MAPPROXY_MAPFILE_DIR)
+        output_dict = update_from_mapfile(input_layers, mapproxy_config, MAPPROXY_MAPFILE_DIR)
         
-    with open("tester2.yaml", "w") as outfile:
-        yaml.dump(output_dict, outfile)
+    print(yaml.dump(output_dict))
 
     
 if __name__ == '__main__':
     cli()
-
-
